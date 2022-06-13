3d4446abc04bd028c5c0e8874dd69d0c73ed5e95
Sebastian <sebastiancooper14@gmail.com>
2022-06-13 11:51:44 -0400

    Map and Player code added

--- Map.cs
+++ Map.cs
@@ -1,7 +1,68 @@
 namespace EscapeTheMaze
 {
-    class Map
+    internal class NewBaseType
     {
+        private string[,] Grid;
+        private int Rows;
+        private int Cols;
+
+        public Map(string[,] grid)
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Cols = Grid.GetLength(1);
+        }
         
+        public void Draw()
+        {
+            for (int y = 0; y < Rows; y++)
+            {
+                for (int x = 0; x < Cols; x++)
+                {
+                    string element = Grid[y, x];
+                    Console.SetCursorPosition(x, y);
+                    if (element == "X")
+                    {
+                        ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Write(element);
+                }
+            }
+        }
     }
-}
+       
+       public string GetElementAt( int x, int y)
+       {
+         return Grid[y, x];
+       }
+
+     public bool IsPositionWalkable(int x , int y)
+     {
+         //Bounds
+      if (x < 0 || y < 0 || x >=Cols || y >= Rows)
+      {
+          return false;
+      }
+       // Is walkable tile
+       return Grid [y,x] == " " || Grid[y, x] == "X";
+
+     }
+
+
+
+
+
+    internal class NewBaseType : NewBaseType
+    {
+    }
+
+    class Map : NewBaseType
+    {
+    
+       }
+    }
--- Player.cs
+++ Player.cs
@@ -2,6 +2,29 @@ namespace EscapeTheMaze
 {
     class Player
     {
+        public int X { get; set; }
+              public int y { get; set;}
+              private string PlayerMarker;
+              private ConsoleColor PlayerColor;
+
+
+        public Player(int initialX, int initialY)
+            {
+              X = initialX;
+              y = initialY;
+              PlayerMarker = "O";
+              PlayerColor = Console.Red;
+
+            }
+
+            public void Draw()
+            {
+              ForegroundColor = PlayerColor;
+              Console.SetCursorPosition(X , Y);
+              Console.Write(PlayerMarker);
+              Console.ResetColor();
+            }
+        
         
     }
 }
